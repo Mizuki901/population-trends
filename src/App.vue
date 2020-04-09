7799c10c57a2b3911344decd3ae50787d417fdf8
hirakawa.mizuki <hirakawa.mizuki.901@gmail.com>
2020-04-09 23:53:06 +0900

    Vue Materialの導入

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <div id="app">
+    <md-toolbar>
+      <h3 class="md-title">Population Trends</h3>
+    </md-toolbar>
     <img src="./assets/logo.png">
     <router-view/>
   </div>
@@ -18,6 +21,6 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 </style>
