e5318ae33b72c324d99715cad394288d8f353c53
hirakawa.mizuki <hirakawa.mizuki.901@gmail.com>
2020-04-11 23:09:37 +0900

    チェックボックスを外したときの処理を追加

--- src/components/pages/Top.vue
+++ src/components/pages/Top.vue
@@ -5,7 +5,7 @@
       <div v-for="prefecture in prefectures" :key="prefecture.prefCode"
         class="pref-item md-layout-item md-xlarge-size-10 md-large-size-15 md-medium-size-15 md-small-size-25 md-xsmall-size-33"
       >
-        <md-checkbox v-model="prefecture.checked" v-on:change="getPopulation(prefecture)">
+        <md-checkbox v-model="prefecture.isChecked" v-on:change="setPopulation(prefecture)">
           {{ prefecture.prefName }}
         </md-checkbox>
       </div>
@@ -17,7 +17,7 @@
       style="width: 90%; height: 600px;"
       :data="populationTrends"
       :options="{scales: {yAxes: [{ticks: {beginAtZero: true}}]}}"
-      :update-config="{duration: 2000, easing: 'easeOutBounce'}"
+      :update-config="{duration: 0, easing: 'easeOutBounce'}"
       type="line"
     />
   </div>
@@ -54,27 +54,46 @@ export default {
       })
   },
   methods: {
-    getPopulation: function (prefecture) {
-      const url = 'https://opendata.resas-portal.go.jp/api/v1/population/sum/estimate?prefCode=' + prefecture.prefCode + '&cityCode=-'
-      axiosWrapper.get(url)
-        .then((res) => {
-          // 都道府県の総人口以外のデータは不要
-          // 1970年から2020年までのデータに整形
-          let populationByYear = res.data.result.data.filter(data => data.label === '総人口')[0].data.slice(2, 13)
-          const population = populationByYear.map((data) => {
-            return data.value
-          })
-
-          // グラフに描画する都道府県を追加
-          const dataset = {
-            backgroundColor: 'rgba(0,0,255,0.1)',
-            borderColor: 'rgba(0,0,255)',
-            borderWidth: 2,
-            data: population,
-            label: prefecture.prefName
-          }
-          this.populationTrends.datasets.push(dataset)
+    setPopulation: function (prefecture) {
+      if (!prefecture.isChecked) {
+        // チェックボックスが外れるときには、その都道府県の人口データは削除する
+        const datasets = this.populationTrends.datasets.filter((data) => {
+          return (data.id !== prefecture.prefCode)
         })
+        this.populationTrends.datasets = datasets
+
+        // prefCode順に都道府県をソートしておく
+        this.populationTrends.datasets.sort(function (data1, data2) {
+          return data1.id - data2.id
+        })
+      } else {
+        const url = 'https://opendata.resas-portal.go.jp/api/v1/population/sum/estimate?prefCode=' + prefecture.prefCode + '&cityCode=-'
+        axiosWrapper.get(url)
+          .then((res) => {
+            // 都道府県の総人口以外のデータは不要
+            // 1970年から2020年までのデータに整形
+            let populationByYear = res.data.result.data.filter(data => data.label === '総人口')[0].data.slice(2, 13)
+            const population = populationByYear.map((data) => {
+              return data.value
+            })
+
+            // グラフに描画する都道府県を追加
+            const dataset = {
+              id: prefecture.prefCode,
+              backgroundColor: 'rgba(0,0,255,0.1)',
+              borderColor: 'rgba(0,0,255)',
+              borderWidth: 2,
+              data: population,
+              label: prefecture.prefName
+            }
+            this.populationTrends.datasets.push(dataset)
+
+            // prefCode順に都道府県をソートしておく
+            this.populationTrends.datasets.sort(function (data1, data2) {
+              return data1.id - data2.id
+            })
+          })
+      }
     }
   }
 }
